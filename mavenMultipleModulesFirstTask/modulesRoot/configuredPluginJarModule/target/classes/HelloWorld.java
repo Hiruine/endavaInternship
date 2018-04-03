bd912567e1056949c895a23e0c29e6594306802a
Radu Madiudin <rmadiudin@endava.com>
2018-04-03 10:37:46 +0300

    jar file works from configuredPluginJarModule

---
+++ mavenMultipleModulesFirstTask/modulesRoot/configuredPluginJarModule/target/classes/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("42");
+
+        int width=10;
+
+        //with lambda
+        Drawable d2=()->{
+            System.out.println("Drawing "+ width);
+        };
+        d2.draw();
+    }
+}
